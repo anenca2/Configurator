5bfd5a633b95d15e5bdd83366f96b690099aad27
Fernando Ayala <ferbayala@gmail.com>
2016-06-25 23:48:59 -0500

    Hola mundo

---
+++ src/main/java/helloPackage/helloworld.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloPackage;
+
+/**
+ *
+ * @author FernandoBadilloAyala
+ */
+
+
+
+public class helloworld {
+    
+    
+    private static String hello(){
+        return "Hello world";
+}
+    
+    public static void main(String[] args) {
+    //Greeter greeter = new Greeter();
+    //System.out.println(greeter.sayHello());
+    System.out.println(hello());
+
+    }
+    
+}
